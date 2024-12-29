dd05b18d385152aab93da2a23fa56fa6d2bd156b
ottyn <97130150+ottyn@users.noreply.github.com>
2024-12-29 14:52:43 -0600

    Add comments to world_scripts

--- world_scripts.js
+++ world_scripts.js
@@ -1,5 +1,7 @@
-// Wait for world to fully load
+// Creates a hook on world load to listen for turn change in a combat encounter.
+// Looks for both weapon and consumable items in actor inventory and deletes if their Qty is 0
 
+// Wait for world to fully load
 Hooks.once('ready', () => {
   Hooks.on('combatTurnChange', async (encounter) => {
     if (encounter.combatant.isOwner) {
@@ -7,7 +9,6 @@ Hooks.once('ready', () => {
       encounter.combatant.actor.itemTypes.weapon
         .filter((item) => item.system.temporary && item.system.quantity === 0)
         .forEach((item) => item.delete());
-
       // Process consumables
       encounter.combatant.actor.itemTypes.consumable
         .filter((item) => item.system.temporary && item.system.quantity === 0)
