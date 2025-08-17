4964b3fec69cb017e844469e1d5d3e9458b2207d
ottyn <97130150+ottyn@users.noreply.github.com>
2025-08-16 19:43:26 -0500

    Update installFoundry.sh
    Add cloudflared to install

--- installFoundry.sh
+++ installFoundry.sh
@@ -46,6 +46,16 @@ read fvttinstance
 pm2 start $HOME/$fvttfolder/resources/app/main.js --name $fvttinstance -- --dataPath=$HOME/$fvttuserdata
 pm2 save
 
+# Add cloudflare gpg key
+sudo mkdir -p --mode=0755 /usr/share/keyrings
+curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
+
+# Add this repo to your apt repositories
+echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared any main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
+
+# install cloudflared
+sudo apt-get update && sudo apt-get install cloudflared
+
 # Restarting the system to complete installation
 sleep 2
 clear
