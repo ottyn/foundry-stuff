8fdd4ed0c74a96739db8515704dc04562b14f584
ottyn <97130150+ottyn@users.noreply.github.com>
2025-08-29 22:31:48 -0500

    Update upgradeNode.sh

--- upgradeNode.sh
+++ upgradeNode.sh
@@ -2,30 +2,25 @@
 # chmod a+x /where/i/saved/it/upgradeNode.sh
 # Upgrade NodeJS on Linux
 
+# Check for system updates and clean packages
 sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean
-
-# To upgrade NodeJS, we should stop pm2 processes
-pm2 stop all
-
-# Remove the current pm2 from startup to allow for the upgrade.
-pm2 unstartup
 
 # Install latest Node Version Manager (NVM) for managing NodeJS versions
 # Change installed version as needed per FoundryVTT documentation.
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 nvm install 22
+nvm use 22
 node -v
 npm -v
 nvm alias default node
 nvm current
 
-# Set pm2 to use the upgraded version of NodeJS and set it to run on start again.
-npm rebuild -g pm2
-pm2 startup
-sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u $USER --hp $HOME
+# Install latest version of pm2 and update the running process.
+npm install pm2@latest -g
+pm2 update   # If process hangs at PM2 Updated, hit Ctrl+C to break out.
 
 # Restart any previously running pm2 managed processes.
-pm2 start all
 pm2 save
+pm2 resurrect
 
 # This concludes the nodejs update.
